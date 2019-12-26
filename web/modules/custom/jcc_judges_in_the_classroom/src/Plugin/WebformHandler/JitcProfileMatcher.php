d6e307feb281f7846c1cb65f9961dade5bd4a898
Ivan Bueno <ivan.bueno@jud.ca.gov>
2019-12-26 13:02:40 -0800

    use token in judge email

--- web/modules/custom/jcc_judges_in_the_classroom/src/Plugin/WebformHandler/JitcProfileMatcher.php
+++ web/modules/custom/jcc_judges_in_the_classroom/src/Plugin/WebformHandler/JitcProfileMatcher.php
@@ -328,13 +328,12 @@ class JitcProfileMatcher extends EmailWebformHandler {
     $emails = implode(',', array_unique($emails));
 
     $token_service = \Drupal::token();
-    $teacher_data = $token_service->replace('[webform_submission:values]', ['webform_submission' => $webform_submission], ['html' => FALSE]);
+    $teacher_data = $token_service->replace('[webform_submission:values]', ['webform_submission' => $webform_submission]);
 
     $message['to_mail'] = $message['from_mail'];
     $message['bcc_mail'] = $emails;
     $message['subject'] = 'A teacher matched your schedule.';
     $message['body'] = $teacher_data;
-    $message['params']['html'] = FALSE;
 
     return $this->sendMessage($webform_submission, $message);
   }
